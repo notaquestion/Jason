1bbafac49836fc7e4a8b5233e5c7facc5f4967c5
Jason <notaquestion@gmail.com>
2021-01-09 20:41:08 -0800

    Ported over the rest of the functions from StacyKeyboard

--- CircutPlaygroundBLE_StacyKeyboardV2.cpp
+++ CircutPlaygroundBLE_StacyKeyboardV2.cpp
@@ -69,24 +69,27 @@ String MenuTreeToString(MenuTree menu)
 //What Menus have sub menus? What MenuTree entries are subsidiary to each?
 MenuTree M_MainMenu[] = { //Can't just be called MainMenu w/o compiler thinking I meant the enum element.
   KeyboardMenu,
-  OptionsMenu
+  OptionsMenu,
+  NoMenu //Serves as a terminatior when itterating through array.
 };
 MenuTree M_OptionsMenu[] = {
   FasterCycleSpeed,
   LongerCycleSpeed,
   ShorterGoBackInput,
-  LongerGoBackInput
+  LongerGoBackInput,
+  NoMenu //Serves as a terminatior when itterating through array.
 };
-
 MenuTree M_KeyboardMenu[] = {
   TypeLetter,
   Backspace,
   AutoComplete,
-  SpecialKeyMenu
+  SpecialKeyMenu,
+  NoMenu //Serves as a terminatior when itterating through array.
 };
 MenuTree M_SpecialKeyMenu[] = {
   Enter,
-  Tab
+  Tab,
+  NoMenu //Serves as a terminatior when itterating through array.
 };
 
 
@@ -211,71 +214,141 @@ void loop() {
         break;
 
     case IdleMenu: 
-        NextCommand = AwaitInput(CycleSpeed);
+        Commands input = AwaitInput(CycleSpeed);
 
-        if(NextCommand == Select1)
+        if(input == Select1)
         {
             DisplayText("Select Pressed, Hold to Use Mouse");
             delay(1000);
             ClearText("Select Pressed, Hold to Use Mouse");
         }
-        else if(NextCommand == Back)
+        else if(input == Back)
         {
             DisplayText("Going to Mouse Menu");
             delay(1000);
             ClearText("Going to Mouse Menu");
 
-            CurrentMenu = MouseMenu;
+            CurrentMenu = MainMenu;
         }
 
         break;
 
     case MainMenu: 
-    
+        Commands input = DisplayMenuOptions(M_MainMenu);
+        if(input == Back)
+        {
+          CurrentMenu = MouseMenu;
+        }
         break;
 
       case KeyboardMenu:
-      
+          Commands input = DisplayMenuOptions(M_KeyboardMenu);
+          if(input == Back)
+          {
+            CurrentMenu = MainMenu;
+          }
           break;
 
         case TypeLetter:
-        
+            String TypeWhat = DispalyGridOptionsAndType(LetterClumps, LetterClumps_SIZE, '/', KeyboardMenu);
+            if(TypeWhat == "_")
+            {
+              TypeWhat = " ";
+              CurrentTypedWord = "";
+              AutoCompleteOptions = "";
+            }
+
+            if(TypeWhat != "")
+            {
+              if(TypeWhat != " ")
+              {
+                CurrentTypedWord += TypeWhat;
+              }
+              DisplayText(TypeWhat);
+              PopulateAutoCompleteDicOptions();
+              CurrentMenu = KeyboardMenu;
+            }
+
             break;
 
         case Backspace: 
-        
+            Keyboard.write(KEY_BACKSPACE);
+            CurrentTypedWord.remove(CurrentTypedWord.length() - 1);
+
+            bool again = true;
+            while(again)
+            {
+              DisplayText("<--REPEAT?--");
+              Commands input = AwaitInput(cycleSpeed);
+
+              if(input == None)
+              {
+                again = false;
+              }
+              else
+              {
+                ClearText("<--REPEAT?--");
+                Keyboard.write(KEY_BACKSPACE);
+                CurrentSelection = "";
+
+                CurrentTypedWord.remove(CurrentTypedWord.length() - 1);
+              }
+              delay(1);
+
+            }
+            ClearText("<--REPEAT?--");
+
+            CurrentMenu = KeyboardMenu;
             break;
 
         case SpecialKeyMenu:
-        
+            Commands input = DisplayMenuOptions(M_SpecialKeyMenu);
+            if(input == Back)
+            {
+              CurrentMenu = KeyboardMenu;
+            }
             break;
 
           case Enter: 
-          
+              Keyboard.write(KEY_RETURN);
+              CurrentMenu = SpecialKeyMenu;
               break;
 
           case Tab:
-      
+              Keyboard.write(KEY_TAB);
+              CurrentMenu = SpecialKeyMenu;
               break;
 
+              //Keyboard.write(KEY_CAPS_LOCK);
+              //Keyboard.write(KEY_ESC);
+
       case OptionsMenu:
-      
+          Commands input = DisplayMenuOptions(M_OptionsMenu);
+
+          if(input == Back)
+          {
+            CurrentMenu = MainMenu;
+          }
           break;
 
         case FasterCycleSpeed:
-        
+            IncreaseCycleSpeed();
+            CurrentMenu = OptionsMenu;
             break;
 
         case LongerCycleSpeed:
-        
+            DecreaseCycleSpeed();
+            CurrentMenu = OptionsMenu;
             break;
 
         case ShorterGoBackInput:
-        
+            ShortenDelay();
+            CurrentMenu = OptionsMenu;
             break;
 
         case LongerGoBackInput:
-    
+            IncreaseDelay();
+            CurrentMenu = OptionsMenu;
             break;
 
     case MouseMenu:
@@ -283,14 +356,14 @@ void loop() {
         break;
 
     default:
-
+        Serial.println("This Current Menu doesnt exist.");
         break;
   }
 
 }
 
 
-//
+//////////////////////////////MOUSE/////////////////////////////////////////
 //This Function gets evaluated every frame. 
 //When the input is not held, MouseTimer is incrimented.
 //  //New Sin/Cos movment values are compute from MouseTimer such that the mouse moves in a circle.
@@ -418,7 +491,7 @@ void MouseFunctions()
 //////////////////////////////INPUT FUNCTIONS/////////////////////////////////
 bool TouchCondition()
 {
-  Serial.print(" CT0("); Serial.print(CircuitPlayground.readCap(0));Serial.print(')');
+  //Serial.print(" CT0("); Serial.print(CircuitPlayground.readCap(0));Serial.print(')');
   return CircuitPlayground.readCap(0) > 1000;
 }
 
@@ -532,6 +605,278 @@ void ClearText (String text_)
   // Serial.println(text_);
 }
 
+void AddCurrsor()
+{
+  Keyboard.write('_');
+  Keyboard.write(' ');
+  Keyboard.write('<');
+  Keyboard.write('-');
+  Keyboard.write('-');
+  Keyboard.write(' ');
+  Keyboard.write(' ');
+}
+
+void RemoveCursor()
+{
+  Keyboard.write(KEY_BACKSPACE);
+  Keyboard.write(KEY_BACKSPACE);
+  Keyboard.write(KEY_BACKSPACE);
+  Keyboard.write(KEY_BACKSPACE);
+  Keyboard.write(KEY_BACKSPACE);
+  Keyboard.write(KEY_BACKSPACE);
+  Keyboard.write(KEY_BACKSPACE);
+}
+
+////////////////////////////LETTER/WORD DISPLAY//////////////////////////////////////
+//(const char* const* array)
+
+Commands DisplayMenuOptions(MenuTree menu_[]) 
+{
+  AddCurrsor();
+
+  Commands doWhat;
+  for(int M = 0; menu_[M] != NoMenu; ++M)
+    {
+      String selectionToDisplay = MenuTreeToString(menu_[M]);
+      
+      delay(100);
+
+      DisplayText(selectionToDisplay);
+
+      // if(M < 6)
+      // {
+      //     strip.setPixelColor(M, PendingColor);         
+      //                      //  Pause for a moment
+      // }
+
+      doWhat = AwaitInput(CycleSpeed); //Will return true when input conditoin true, or false in 1000 frames.
+      
+      delay(100);
+
+      delay(100);
+
+      ClearText(selectionToDisplay);
+      
+      if(doWhat == Select1)
+      {
+        NextMenu = menu_[M];
+
+        RemoveCursor();
+        return doWhat;
+      }
+      else if(doWhat == Back)
+      {
+        RemoveCursor();
+        return doWhat;
+      }
+      
+    }
+    RemoveCursor();
+    return doWhat;
+}
+
+String DispalyGridOptionsAndType(const char* const* options, int size, char seperator, MenuTree goBackMenu)
+{
+    String selection = "";
+    AddCurrsor();
+
+    for(int g = 0; g < size; ++g)
+    {
+
+        String optionsString = String(options[g]); //String cast not needed?
+        DisplayText(" >");
+        DisplayText(optionsString);
+        DisplayText("<");
+
+        Commands doWhatNow = AwaitInput(CycleSpeed);
+        
+        ClearText(" >");
+        ClearText(optionsString);
+        ClearText("<");
+
+        if(doWhatNow == Select1)
+        {
+            selection = ParseStringAndPresentOptions(optionsString, seperator);
+           
+           if(selection == "")
+           {
+            //We wanted to go back.
+            g = -1; //This'll become 0 in the next go round.
+           }
+           else
+           {
+            g = size ;
+            //delay(3000);
+           }
+
+        }
+        else if(doWhatNow == Back)
+        {
+          CurrentMenu = goBackMenu;
+          //g = size;
+          RemoveCursor();
+          return "";
+        }
+
+        if(g == (size - 1))
+        {
+          g = -1;
+        }
+    }
+    RemoveCursor();
+
+    if(selection.length() > 1)
+    {
+      selection = " " + selection + " ";
+    }
+
+    if(selection != "")
+    {
+      //DisplayText(selection);
+      return selection;
+    }
+}
+
+String ParseStringAndPresentOptions(String parseMe, char seperator)
+{
+  String GridSelection;
+  Commands doWhatNow;
+
+  int c = 0;
+  String chocies[10];
+
+  int p = 0;
+  String CurrentString = "";
+
+  while(parseMe[p] != '\0' && c < 10)
+  {
+    if(parseMe[p] == seperator)
+    {
+      chocies[c] = CurrentString;
+      ++c;
+      CurrentString = "";
+    }
+    else
+    {
+      CurrentString += parseMe[p];
+    }
+
+    if(parseMe[p + 1] == '\0')
+    {
+      chocies[c] = CurrentString;
+      ++c;
+      CurrentString = "";
+    }
+
+    ++p;
+  }
+  
+
+  // for(int s = 0; s < c; ++s) //We're going to offer you the choice of each thing in choices[]. 
+  // {
+  int s = 0;
+  while(true)
+  {
+    DisplayText(">");
+    for(int w = 0; w < c; ++w) //We're going to write each option in choices.
+    {
+      if(s == w)
+      {
+            DisplayText(" |");
+            DisplayText(chocies[w]);
+            DisplayText("| /");
+      }
+      else
+      {
+        DisplayText(chocies[w]);
+        DisplayText("/");
+      }
+      // Keyboard.write(options[g][L]);
+    }
+    DisplayText("<");
+
+    doWhatNow = AwaitInput(CycleSpeed);
+
+    if(doWhatNow == Select1)
+    {
+      GridSelection =  chocies[s];
+
+      CurrentMenu = KeyboardMenu;
+      s = c;
+    }
+    else if(doWhatNow == Back)
+    {
+       GridSelection = "";
+       s = c;
+    }
+
+    ClearText("> || <");
+    for(int clearWord = 0; clearWord < c; ++clearWord) //We're going to clear
+    {
+      ClearText(chocies[clearWord]);
+      // if(clearWord != s)
+      // {
+        ClearText("/");
+      // }
+    }
+
+    ++s;
+    if(s == c)
+      s = 0;
+
+    if(doWhatNow == Select1 || doWhatNow == Back)
+    {
+      return GridSelection;
+    }
+
+    // for(int d = 0; d < 11 ; ++d)
+    // {
+    //   Keyboard.write(KEY_BACKSPACE);
+    // }
+
+
+  }
+}
+
+
+/////////////////////////////ONBOARD OPTIONS/SETTINGS FUNCTIONS//////////////////////////////////////
+void ShortenDelay()
+{
+  GoBackHoldTime -= 250;
+  String message = String("  --  Hold Time for Back: " + String(GoBackHoldTime));
+  DisplayText(message);
+  delay(2000);
+  ClearText(message);
+  //Serial.println(CycleSpeed);
+}
+
+void IncreaseDelay()
+{
+  GoBackHoldTime += 500;
+  String message = String("  --  Hold Time For Back: " + String(GoBackHoldTime));
+  DisplayText(message);
+  delay(2000);
+  ClearText(message);
+  //Serial.println(CycleSpeed);
+}
+
+void IncreaseCycleSpeed()
+{
+  CycleSpeed *= 0.9;
+  String message = String("  --  CycleSpeed: " + String(CycleSpeed));
+  DisplayText(message);
+  delay(2000);
+  ClearText(message);
+}
+
+void DecreaseCycleSpeed()
+{
+  CycleSpeed *= 1.2;
+  String message = String("  --  CycleSpeed: " + String(CycleSpeed));
+  DisplayText(message);
+  delay(2000);
+  ClearText(message);
+}
 
 
 /////////////////////////////LED FUNCTIONS//////////////////////////////////////
